eb82d60020d5a09253598e150afb0ec7dba706b9
bboyKamel <nacoon@tlen.pl>
2018-10-28 12:30:22 +0100

    multi operation

--- src/main/java/com/sda/Calc.java
+++ src/main/java/com/sda/Calc.java
@@ -1,4 +1,77 @@
 package com.sda;
 
-public class Calc {
+import org.apache.commons.lang3.StringUtils;
+
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.io.PrintWriter;
+import java.util.Optional;
+
+public class Calc extends HttpServlet {
+
+    @Override
+    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+
+        Integer a = Optional.ofNullable(req.getParameter("a"))
+                .filter(e -> StringUtils.isNumeric(e))
+                .map(e -> Integer.valueOf(e))
+                .orElse(0);
+        Integer b = Optional.ofNullable(req.getParameter("b"))
+                .filter(e -> StringUtils.isNumeric(e))
+                .map(e -> Integer.valueOf(e))
+                .orElse(0);
+
+        CalculationResult result = calculate(req.getPathInfo(), a, b);
+
+
+        PrintWriter writer = resp.getWriter();
+        writer.print("<h1>Wynik " + result.resultRepresentation + "</h1>");
+
+
+    }
+
+    private CalculationResult calculate(String path, int a, int b) {
+
+        if (path.endsWith("add")) {
+            return new CalculationResult(a + b, a + " + " + b + " = " + (a + b));
+
+        } else if (path.endsWith("substract")) {
+            return new CalculationResult(a - b, a + " - " + b + " = " + (a - b));
+        } else if (path.endsWith("multiply")) {
+            return new CalculationResult(a * b, a + " - " + b + " = " + (a * b));
+        }
+        return new CalculationResult( 0, "Zły operator");
+    }
+
+    private static class CalculationResult {
+        private Integer result;
+        private String resultRepresentation;
+
+        public CalculationResult(Integer result, String resultRepresentation) {
+            this.result = result;
+            this.resultRepresentation = resultRepresentation;
+        }
+
+        public Integer getResult() {
+            return result;
+        }
+
+        public void setResult(Integer result) {
+            this.result = result;
+        }
+
+        public String getResultRepresentation() {
+            return resultRepresentation;
+        }
+
+        public void setResultRepresentation(String resultRepresentation) {
+            this.resultRepresentation = resultRepresentation;
+        }
+    }
 }
+
+
+
--- src/main/java/com/sda/HelloServlet.java
+++ src/main/java/com/sda/HelloServlet.java
@@ -1,5 +1,7 @@
 package com.sda;
 
+import org.apache.commons.lang3.StringUtils;
+
 import javax.servlet.ServletException;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
@@ -12,8 +14,10 @@ public class HelloServlet extends HttpServlet {
     @Override
     protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
 
-        PrintWriter writer = resp.getWriter();
+        String name = req.getParameter("name");
+        String nameToDisplay = StringUtils.isEmpty(name) ? "Annonymous" : name;
 
-        writer.print("<h1>Hello World</h1>");
+        PrintWriter writer = resp.getWriter();
+        writer.print("<h1>Hello " + nameToDisplay + "</h1>");
     }
 }
